53fb49db602dde3f3b151129e7528e59e5c430bb
Gaurav Pratap <gauravpratapsingh1071997@gmail.com>
2025-10-04 11:54:10 +0530

    fix errors

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Performance optimizations
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['@next/font'],
-  },
-  
   // Image optimization
   images: {
     formats: ['image/webp', 'image/avif'],
